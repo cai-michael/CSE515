a88917a735bc82e3f698058efec94ef12e34c33c
Ajide49 <ausman3@asu.edu>
2020-10-25 00:00:49 -0700

    refactor to read in input from task 3

--- phase2/task4a.py
+++ phase2/task4a.py
@@ -1,55 +1,32 @@
-import task3_util as util
-from task2 import options
-import numpy as np
-from sklearn import decomposition
+import os
 
 # Main
 if __name__ == '__main__':
-    # Load arguments
-    p = int(input('How many principle components to return: '))
-
-    # List options
-    print('Options:')
-    for option_number, (option_name, _, _) in options.items():
-        print(f'{option_number}. {option_name}')
-
-    option = int(input('Please select an option: '))
-    while option not in options:
-        print('Invalid option. Please try again.')
-        option = int(input('Please select an option: '))
-
-    vector_model = None
-
-    if option < 6:
-        vector_model = input('Please select a vector model (TF/TF-IDF): ')
-        while vector_model not in { 'TF', 'TF-IDF' }:
-            print('Invalid vector model. Please try again.')
-            vector_model = input('Please select a vector model (TF/TF-IDF): ')
-    
-    similarityMatrix, gestureIndexes = util.createSimilarityMatrix(vector_model, option)
-
-    # Perform SVD
-    print("Found Gesture-Gesture Similarity Matrix Performing SVD\n")
-    model = decomposition.TruncatedSVD(n_components=p)
-    svdDecomp = model.fit(similarityMatrix)
-    # Find top-p principle components
-    basisVectors = svdDecomp.components_
-
-    # Find the contributions of each gesture to each basis vectors
     gestureHighestDegreeOfMembershipDictionary = {}
-    for i in range(p):
-        latentComponentContributions = basisVectors[i]
-        for gesture, contribution in zip(gestureIndexes, latentComponentContributions):
-            currentComponentContributionPair = (i, abs(contribution))
-            if gestureHighestDegreeOfMembershipDictionary.get(gesture) != None:
-                maximumComponentContributionPair = gestureHighestDegreeOfMembershipDictionary.get(gesture)
-                gestureHighestDegreeOfMembershipDictionary[gesture] = maximumComponentContributionPair if maximumComponentContributionPair[1]\
-                    > currentComponentContributionPair[1] else currentComponentContributionPair
-            else:
-                gestureHighestDegreeOfMembershipDictionary[gesture] = currentComponentContributionPair
-   
-    # Print
-    print("Partitioning gestures into " + str(p) + " groups based on degree of membership")
+    directory = "principal_components"
+    # Read in b_components file from directory
+    for component_file in os.listdir(directory):
+        if "a" in component_file:
+            print("Reading in "  + component_file + " file from Task 3a output \n")
+            with open(directory + "/" + component_file) as f:
+                for line in f:
+                    gesture_component_combination = line.split("|")
+                    p_component = int(gesture_component_combination[0])
+                    gesture = int(gesture_component_combination[1])
+                    similarity_score = float(gesture_component_combination[2])
+                    currentComponentContributionPair = (p_component, similarity_score)
+
+                    # Create gesture dictionary with highest degree of membership
+                    if gestureHighestDegreeOfMembershipDictionary.get(gesture) != None:
+                        maximumComponentContributionPair = gestureHighestDegreeOfMembershipDictionary.get(gesture)
+                        gestureHighestDegreeOfMembershipDictionary[gesture] = maximumComponentContributionPair if maximumComponentContributionPair[1]\
+                            > currentComponentContributionPair[1] else currentComponentContributionPair
+                    else:
+                        gestureHighestDegreeOfMembershipDictionary[gesture] = currentComponentContributionPair
+  
+    p = p_component + 1
+    # Print results
+    print("Partitioning gestures into " + str(p) + " groups based on degree of membership\n")
     for i in range(p):
         print(f"Gestures With Highest Degree of Membership for Principal Component {i + 1}:")
         print("Gesture\t|\tScore")
