ccba72e88e76b2fc84f4976abded6d508a0be9b7
Ajide49 <ausman3@asu.edu>
2020-11-24 21:38:55 -0700

    change task5 to function and add score printing

--- phase3/task3.py
+++ phase3/task3.py
@@ -1,6 +1,7 @@
 from task3_util import load_vector, load_vectors, LSH
 from pprint import pprint
-import os
+from task4 import probabilistic_relev
+
 
 if __name__ == '__main__':
     L = int(input('Please enter the number of layers L: '))
@@ -20,23 +21,22 @@ if __name__ == '__main__':
     t = int(input('Please enter t: '))
 
     query = load_vector(vector_model, gesture_id)
-    #print("query is", query)
-    working_dir = os.getcwd()
-    folder_name = 'lsh_outputs'
-    file_name = 'task3_results'
-    if (not folder_name in os.listdir(working_dir)):
-	    os.mkdir(working_dir + '/' + folder_name)
+
     # Find t most similar gestures
     top_t, no_buckets, no_unique, overall_no = lsh.find_t_most_similar(query, t)
-    with open(f'{folder_name}/{file_name}', 'w') as f:
-        print('No. of buckets searched: ', no_buckets)
-        f.write(str(no_buckets) + " | ")
-        print('No. of unique gestures considered: ', no_unique)
-        f.write(str(no_unique)  + " | ")
-        print('Overall no. of gestures considered: ', overall_no)
-        f.write(str(overall_no))
-        f.write("\n")
-        print(f'Top {t} most similar gestures:')
-        for index, (gesture_id, distance) in enumerate(top_t):
-            print(f'{index + 1}.\t{gesture_id}\t(distance={distance})')
-            f.write(f'{index + 1}\t{gesture_id}\t{distance}\n')
+
+    print('No. of buckets searched: ', no_buckets)
+    print('No. of unique gestures considered: ', no_unique)
+    print('Overall no. of gestures considered: ', overall_no)
+
+    print(f'Top {t} most similar gestures:')
+    for index, (gesture_id, distance) in enumerate(top_t):
+        print(f'{index + 1}.\t{gesture_id}\t(distance={distance})')
+
+
+    # temp until task 6
+    #relevant = int(input())
+    #nonrelevant = int(input())
+    #nonrelevant2 = int(input())
+    #results = [x for x,_ in top_t]
+    #probabilistic_relev(lsh, vector_model, query, results, [results[relevant - 1]], [results[nonrelevant - 1], results[nonrelevant2 - 1]])
--- phase3/task5.py
+++ phase3/task5.py
@@ -1,66 +1,53 @@
 import general_util as util
 from graph_util import *
 import os
+import operator
 
-task3_dir = "lsh_outputs"
-working_dir = os.getcwd()
-top_t_gestures = []
-# Retrieve top t gestures form task3
-with open(task3_dir + "/task3_results") as f:
-    for line in f:
-        if "|" not in line:
-            task3_results = line.split()
-            top_t_gestures.append(task3_results[1])
-    print("Top t gestures results from task3:")
-for i in range(len(top_t_gestures)):
-    print(top_t_gestures[i])
-print("\n")
 
-# Assign gestures to be relevant or irrelevant
-print('Which gestures would you like to list as relevant? (e.g. 1, 249, 559, etc.):')
-relevant_gestures = input().replace(' ', '').split(',')
-print('Which gestures would you like to list as irrelevant? (e.g. 2, 258, 537, etc.):')
-irrelevant_gestures = input().replace(' ', '').split(',')
+def classifier_revel(top_t_gestures, relevant, irrelevant):
+    working_dir = os.getcwd()
 
-#PPR
-# Retrieve similiarity matrix, graph, and data files to use in calculation
-data_files, similarity_matrix = util.read_similarity_matrix(working_dir + util.SLASH + util.GRAPH_FOLDER + util.SLASH + 'similarity_matrix.txt')
+    #Personalized Page Rank
+    # Retrieve similiarity matrix, graph, and data files to use in calculation
+    data_files, similarity_matrix = util.read_similarity_matrix(working_dir + util.SLASH + util.GRAPH_FOLDER + util.SLASH + 'similarity_matrix.txt')
 
-similarity_graph = util.read_similarity_graph(working_dir + util.SLASH + util.GRAPH_FOLDER + util.SLASH + 'similarity_graph.txt')
+    similarity_graph = util.read_similarity_graph(working_dir + util.SLASH + util.GRAPH_FOLDER + util.SLASH + 'similarity_graph.txt')
 
-# remove irrelevant gestures from similarity_graph
-for gesture in irrelevant_gestures:
-    for g in similarity_graph:
-        if gesture in similarity_graph[g]:
-            similarity_graph[g].pop(similarity_graph[g].index(gesture))
+    # remove irrelevant gestures from similarity_graph
+    for gesture in irrelevant_gestures:
+        for g in similarity_graph:
+            if gesture in similarity_graph[g]:
+                similarity_graph[g].pop(similarity_graph[g].index(gesture))
 
-# Build transition matrix
-transition_matrix = create_transition_matrix(similarity_graph)
+    # Build transition matrix
+    transition_matrix = create_transition_matrix(similarity_graph)
 
-# initialize seed gestures with the relevant gestures
-seed_gestures = list(filter(lambda a : a in data_files, relevant_gestures))
-seed_gestures = list(set(seed_gestures)) # remove redundancies
+    # initialize seed gestures with the relevant gestures
+    seed_gestures = list(filter(lambda a : a in data_files, relevant_gestures))
+    seed_gestures = list(set(seed_gestures)) # remove redundancies
 
-# Compute dominant gestures
-init_vector = np.zeros((len(data_files), 1))
-for gesture in seed_gestures:
-    init_vector[data_files.index(gesture), 0] = 1.0/len(seed_gestures)
+    # Compute dominant gestures
+    init_vector = np.zeros((len(data_files), 1))
+    for gesture in seed_gestures:
+        init_vector[data_files.index(gesture), 0] = 1.0/len(seed_gestures)
 
-ppr = personalized_page_rank(transition_matrix, init_vector, 0.1)
-gesture_score_dict = {}
-gesture_index_in_graph = list(similarity_graph.keys())
+    ppr = personalized_page_rank(transition_matrix, init_vector, 0.1)
+    gesture_score_dict = {}
+    gesture_index_in_graph = list(similarity_graph.keys())
+    dominant_gestures = ppr.transpose().tolist()[0]
+    for index in range(len(dominant_gestures)):
+        score = dominant_gestures[index] # the score from PPR
+        gesture_name = gesture_index_in_graph[index]
+        if gesture_score_dict.get(gesture_name): # check if this gesture is in the dict
+            if gesture_score_dict[gesture_name] < score:
+                gesture_score_dict[gesture_name] = score
+        else:
+            gesture_score_dict[gesture_name] = score
 
-dominant_gestures = ppr.transpose().tolist()[0]
-dominant_gestures = sorted(range(len(dominant_gestures)), reverse=True, key=lambda a : dominant_gestures[a])
-dominant_gestures = [data_files[a] for a in dominant_gestures]
+    sorted_gesture_scores = dict(sorted(gesture_score_dict.items(), key=operator.itemgetter(1), reverse=True))
+    order = 0
+    print(f'Classifier-based relevance feedback:')
+    for gesture_id, score in sorted_gesture_scores.items():
+        if gesture_id in top_t_gestures:
+            print(f'{order + 1}.\t{gesture_id}\t(score={score})')
 
-#print(dominant_gestures[10])
-#print(gesture_score_dict)
-print(f'Classifier-based relevance feedback:')
-for gesture in dominant_gestures:
-    if gesture in top_t_gestures:
-        print(gesture)
-# get task 3 output, user will mark which ones are relevant and irreleveant
-# revelant <- uses for the vector nodes in PPR
-# irrelevant <- remove them from the similarity matrix
-# print out the new strongest components
