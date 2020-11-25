ea0bf4416984e6babdc6052859c56238926e78bd
Ajide49 <ausman3@asu.edu>
2020-11-24 20:44:19 -0700

    add task4 to task6

--- phase3/task6.py
+++ phase3/task6.py
@@ -5,13 +5,13 @@ import general_util as util
 from task2_util import *
 from task3_util import load_vector, load_vectors, LSH
 from graph_util import *
+from task4 import probabilistic_relev
 
 working_dir = os.getcwd()
 
 L = int(input('Please enter the number of layers L: '))
 k = int(input('Please enter the number of hashes per layer k: '))
 vector_model = input('Please enter a vector model (TF/TF-IDF): ')
-feedbackModel = input('Please choose a feedback system to utilize:\n1. Probabilistic Relevance Feedback\n2. Classifier-Based Relevance Feedback')
 
 # Load vectors from vector_data folder
 vectors, vector_ids = load_vectors(vector_model)
@@ -39,6 +39,8 @@ for index, (gesture_id, distance) in enumerate(top_t):
     top_t_gestures.append(gesture_id)
     
 user_choice = 0
+relevant_gestures = []
+irrelevant_gestures = []
 while user_choice != 4:
     print("Displaying Relevant Gestures")
     # Find t most similar gestures
@@ -50,7 +52,7 @@ while user_choice != 4:
     print(f'Top {t} most similar gestures:')
     for index, (gesture_id, distance) in enumerate(top_t):
         print(f'{index + 1}.\t{gesture_id}\t(distance={distance})')
-    print("\nPick an option:\n1. Give Feedback\n2. Apply Probabilistic Revelance Feedback\n3. PPR Relevance Feedback\n4. Quit\n")
+    print("\nPick an option:\n1. Give Feedback\n2. Apply Probabilistic Revelance Feedback\n3. Classifier-Based Relevance Feedback\n4. Quit\n")
     user_choice = input()
     if user_choice == 1:
         print('Which gestures would you like to list as relevant? (e.g. 1, 249, 559, etc.):')
@@ -59,7 +61,7 @@ while user_choice != 4:
         irrelevant_gestures = input().replace(' ', '').split(',')
     elif user_choice == 2:
         print("Re-Running the Query with the new feedback...")
-        #call function for task4
+        probabilistic_relev(lsh, vector_model, query, top_t_gestures, relevant_gestures, irrelevant_gestures)
     elif user_choice == 3:
         print("Re-Running the Query with the new feedback...")
         #call function for task5
